77c303f26124a504ae4ba7f3685f808432c6c132
Tanya Bozhok <lady.bozho4ek@gmail.com>
2023-11-02 19:37:56 +0300

    Ввод кол-ва элементов в первичном массиве

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//длина которых меньше, либо равна 3 символам.
+
+Console.Write("Введите количество элементов в массиве от 1 до 10: ");
+
+int countElements = Convert.ToInt32(Console.ReadLine());
+
+bool validateInputCountElement = ValidateInputCountElement(countElements);
+if (! validateInputCountElement) {
+    Console.WriteLine("Кол-во элементов должно быть от 1 до 10");
+    return;
+}
+
+bool ValidateInputCountElement(int validateValue)
+{
+    if (validateValue > 10 || validateValue < 1)
+    {
+        return false;
+    }
+
+    return true;   
+}
